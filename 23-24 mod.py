909a11441e1e4ffb3cab3fa1cdfecf9d5b579b19
joxa066 <joxay82@gmail.com>
2024-04-20 13:53:51 +0500

    homework 23-24 mod

---
+++ 23-24 mod.py
@@ -0,0 +1,15 @@
+# class User:
+#     def __init__(self, ism, foydalanuvchi_ismi, email, vahokazo):
+#         self.ism = ism
+#         self.foydalanuvchi_ismi = foydalanuvchi_ismi
+#         self.email = email
+#         self.vahokazo = vahokazo
+#
+#     def get_info(self):
+#         info = f"Foydalanuvchi: {self.foydalanuvchi_ismi}, Ismi: {self.ism}, Email: {self.email}"
+#         return info
+#
+#
+# user = User("Alijon Valiyev", "alijon1994", "alijon1994@gmail.com", "vahokazo")
+# info = user.get_info()
+# print(info)
